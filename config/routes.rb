4ce7c89c7f2ad2fd8d04a2a81ed4f6b296e0e895
Romario Sanchez <romario.s17@protonmail.com>
2020-11-26 10:55:01 -0500

    navibar

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,18 @@
 Rails.application.routes.draw do
-  get 'likes/create'
-  get 'likes/destroy'
-  get 'dashboards/index'
-  get 'dashboards/settings'
-  get 'dashboards/show'
-  get 'dashboards/likes'
+  # get 'likes/create'
+  # get 'likes/destroy'
+  # get 'dashboards/index'
+  # get 'dashboards/settings'
+  # get 'dashboards/show'
+  # get 'dashboards/likes'
   devise_for :users
   root to: 'dashboard#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get '/dashboard', to: 'dashboard#index', as: :dashboard
   get '/settings', to: 'dashboard#settings'
+  get 'dashboard/likes', to: "dashboard#likes"
   get '/dashboard/:id', to: 'dashboard#show'
   post 'likes/:id', to: "likes#create"
   delete 'likes/:id', to: "likes#destroy"
-  get 'dashboard/likes', to: "dashboard#likes"
 end
