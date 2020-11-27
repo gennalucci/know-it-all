0cf7bd943ea35704b736d3fb160461bf6ace9db7
Romario Sanchez <romario.s17@protonmail.com>
2020-11-27 14:45:13 -0500

    cards n routes stuff

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # get 'dashboards/settings'
   # get 'dashboards/show'
   # get 'dashboards/likes'
-  devise_for :users
+  devise_for :users, :controllers => {:registrations => "registrations"}
   root to: 'dashboards#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
@@ -13,14 +13,14 @@ Rails.application.routes.draw do
   get '/settings', to: 'dashboards#settings', as: :settings
   get 'dashboards/likes', to: "dashboards#likes", as: :user_likes
   get '/dashboards/:id', to: 'dashboards#show', as: :article_show
-  get '/setup', to: 'dashboards#setup'
+  get '/setup', to: 'dashboards#setup', as: :setup
   post 'likes/:id', to: "likes#create", as: :like_article
   delete 'likes/:id', to: "likes#destroy", as: :unlike_article
 
   resources :user_tags, only: [ :new, :create, :destroy ]
   get '/user_tags/create', to: 'user_tags#create_tags'
 
-  get '/time', to: 'dashboards#time'
+  get '/time', to: 'dashboards#time', as: :time
 
 
 end
