9a260a62da0a99d323aa359ac2a38738d471b4e3
Berlin Onumonu <berlin.onumonu@gmail.com>
2020-12-01 09:09:13 -0500

    updated javascript

--- app/javascript/components/init_topics.js
+++ app/javascript/components/init_topics.js
@@ -1,53 +1,44 @@
 const buildHtml = ((element, tagContainer) => {
-  // console.log(element.innerText);
   const tagId = element.dataset.elementId;
   const newButton = `<button class="tag-button selected-tag topic-tag-button" data-element-id="${tagId}" data-element="tag">${element.innerText}</button>`;
   tagContainer.insertAdjacentHTML('beforeend', newButton);
   element.classList.add("tag-hidden");
 });
-
 const selectTags = (wrappers) => {
   wrappers.forEach((wrapper) => {
     const buttons = wrapper.querySelectorAll(".topic-tag-button");
     const wrapperName = wrapper.dataset.topicWrapper;
     const tagContainer = wrapper.querySelectorAll(`[data-topic-tags='${wrapperName}']`);
-
-    // console.log(tagContainer);
-
     buttons.forEach((button) => {
       button.addEventListener('click', (e) => {
         const element = e.currentTarget;
-        // element.classList.toggle("selected");
         buildHtml(element, tagContainer[0]);
       });
     });
   });
 };
-
+const selectTopics = (elements) => {
+  elements.forEach((el) => {
+    el.addEventListener('click', (e) => {
+      e.currentTarget.classList.toggle('selected');
+    });
+  })
+};
 const initTopics = () => {
   const topicTagContainer = document.getElementById("topic-tag-container");
-
   if (topicTagContainer){
     const url = window.location.origin;
     const elements = topicTagContainer.querySelectorAll(".topic-tag-button");
-
+    if (elements.length > 0) { selectTopics(elements); }
     const wrappers = document.querySelectorAll("[data-topic-wrapper]");
-    selectTags(wrappers);
-
-    // const tagContainers = topicTagContainer.querySelectorAll(".selected-tags");
-
+    if (wrappers.length > 0) { selectTags(wrappers); }
     //Add event listener to next button
     const nextButton = document.getElementById("btn-next");
     nextButton.addEventListener("click", (e) => {
-      const selectedElements = topicTagContainer.querySelectorAll(".selected-tag");
-      console.log(selectedElements);
-
-
-
+      const selectedElements = topicTagContainer.querySelectorAll(".selected");
       const elementIds = Array.from(selectedElements).map(element => {
         return element.dataset.elementId;
       });
-
       if (selectedElements[0].dataset.element === "topic") {
         window.location.replace(`${url}/user_tags/new?elementIds=${elementIds.join(',')}`);
       } else {
@@ -56,13 +47,4 @@ const initTopics = () => {
     })
   }
 }
-
-// Check if html file has container-id
-// if yes, then event listener on li's
-// toggle class when user selects topic
-// add event listener on next button
-// find all buttons selected (by class)
-// pull data-set-id from all buttons selected by user
-// create a url containing all IDs
-// send user to next page/url where they can choose tags
 export { initTopics };
